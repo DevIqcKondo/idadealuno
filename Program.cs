7ed8b0b2e094b28eed46d170f5dcd97a77c29b2e
Isadora Kondo <kondoiqcdev@gmail.com>
2021-05-21 19:45:11 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace idadealuno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nascimento = new DateTime(2001, 9, 23);
+            DateTime dezoitoanos = new DateTime(2019, 9, 23);
+            Console.WriteLine("Eu nasci em "+nascimento.ToLongDateString());
+            Console.WriteLine("Meu aniversário de 18 anos foi em "+dezoitoanos.ToLongDateString());
+        }
+    }
+}
